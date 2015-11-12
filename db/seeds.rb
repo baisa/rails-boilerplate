adbbfd2be06680b063d25f8ad2a387d5312f6773
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-11-12 12:25:03 +0100

    models: author, book, authorship

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Dane w seed - np 3 ksiazki i 3 autorow. Niech przynajmniej jedna ksiazka ma wiecej niz jednego autora
+
+books = Book.create([{title: 'Anna Karenina', description: 'classic book', author: [authors.first, authors.last]},
+ {title: 'War and Peace', description: 'historical book', author: authors.first},
+ {title: 'The Ruby Way', description: 'IT book', author: authors.last}])
