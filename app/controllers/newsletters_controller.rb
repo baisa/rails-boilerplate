ff459b277f159d2b18357943e17e8ac95fea2b62
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-12-01 12:12:16 +0100

    mailer for one subscriber

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -61,6 +61,7 @@ class NewslettersController < ApplicationController
     end
   end
 
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_newsletter
---
+++ app/mailers/subscriber_mailer.rb
@@ -0,0 +1,10 @@
+class SubscriberMailer < ApplicationMailer
+  default from: "yourtravelapp@gmail.com"
+  
+  def newsletter(subscriber)
+    @subscriber = subscriber
+
+    mail to: subscriber.email, subject: "Our new newsletter"
+  end
+end
+#:bcc - Who you would like to Blind-Carbon-Copy on this email, can be a string of addresses, or an array of addresses.
