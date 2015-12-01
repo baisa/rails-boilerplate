46e55d8fdc1c2c08090d5af4c2d268f799b15e6d
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-12-01 12:30:47 +0100

    sending mail with cc copy

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -4,7 +4,7 @@ class SubscriberMailer < ApplicationMailer
   def newsletter(subscriber)
     @subscriber = subscriber
 
-    mail to: subscriber.email, subject: "Our new newsletter"
+    mail to: subscriber.email, subject: "Our new newsletter", bcc: ["barbarajoannabusse@gmail.com", "barbarajoannaslawinska@gmail.com"]
   end
 end
 #:bcc - Who you would like to Blind-Carbon-Copy on this email, can be a string of addresses, or an array of addresses.
