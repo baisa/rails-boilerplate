d12f6550d22204cb3e6405fd52b98e924075564b
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-12-01 11:20:41 +0100

    Newsletter model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127192613) do
+ActiveRecord::Schema.define(version: 20151201101750) do
+
+  create_table "newsletters", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "subscribers", force: :cascade do |t|
     t.string   "name"
