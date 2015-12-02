5c3f4312361fdf11679fc81801d00eda9ace485f
Barbara Slawinska <slawinskabarbara@gmail.com>
2015-12-02 21:57:08 +0100

    removing default sending, moving sending to method

--- app/mailers/subscriber_mailer.rb
+++ app/mailers/subscriber_mailer.rb
@@ -1,12 +1,7 @@
 class SubscriberMailer < ApplicationMailer
- 
   default from: "yourtravelapp@gmail.com"
 
-  default to: Subscriber.all.collect(&:email).join(",")
-  
-#map { |sub| sub.email }
   def newsletter(subscriber)
-    @subscriber = subscriber
-    mail subject: "Our new newsletter"
+    mail to: Subscriber.all.collect(&:email).join(","), subject: "Our new newsletter"
   end
 end
