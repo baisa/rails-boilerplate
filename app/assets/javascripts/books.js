09f1a8f57db0b54f37b9bbe3e12bdd06c2def5c6
Barbara Slawinska <slawinskabarbara@gmail.com>
2016-01-09 19:21:26 +0100

    correction to input token

---
+++ app/assets/javascripts/books.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#book_author_tokens").tokenInput("/authors.json", {
+    theme: 'facebook',
+    prePopulate: $('#book_author_tokens').data('load')
+  });
+});
+
--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151112083838) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "slug"
-  end
-
-  add_index "articles", ["slug"], name: "index_articles_on_slug"
-
   create_table "authors", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -44,16 +34,6 @@ ActiveRecord::Schema.define(version: 20151112083838) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "oauth_token"
-    t.datetime "oauth_expires_at"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
   end
 
 end
